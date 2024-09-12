7b6165d159fdc546ef8dfa670821e751b7a0f7e9
Hababah <olaleyehabeebullah13@gmail.com>
2024-09-12 21:32:51 +0100

    Script that perform simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,9 @@
 #Script that calculates the simple interest earned on an investment
-principal = 1000
-rate = 0.05
-time = 3
+principal = 1000  # Principal Amount (P)
+rate = 0.05       # Annual interest rate (R)
+time = 3          # Time in years (I)
 result = principal * rate * time
-interest = result
+interest = result    # The formula for simple interest is (I = P * R * T)
 
 print("For principal = 1000, rate = 0.05, and time = 3")
 print("The simple interest is: ",float(interest))
