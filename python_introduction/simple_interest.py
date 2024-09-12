76b5ea7e8fdd7af230589a6f3358686da12a779a
Hababah <olaleyehabeebullah13@gmail.com>
2024-09-12 21:05:48 +0100

    Script that perform simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,7 +2,8 @@
 principal = 1000
 rate = 0.05
 time = 3
-interest = float(principal * rate * time)
+result = principal * rate * time
+interest = float(result)
 
 print("For principal = 1000, rate = 0.05, and time = 3")
 print("The simple interest is: ",interest)
