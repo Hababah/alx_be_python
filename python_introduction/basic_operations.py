38d64b60bb8e8d43e3c88de6296ba4fd0ccf3b5c
Hababah <olaleyehabeebullah13@gmail.com>
2024-09-12 19:31:22 +0100

    Basic Arithmetic in Python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+#Script that perform basic arithmetic operations with two predefined numbers.
+number1 = 10
+number2 = 5
+sum = number1 + number2
+Difference = number1 - number2
+product = number1 * number2
