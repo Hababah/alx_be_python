2a3ca391b9ad9c9a1219698ff1eec125a4b56b86
Hababah <olaleyehabeebullah13@gmail.com>
2024-09-25 19:28:32 +0100

    Script that recommend weather program

--- simple_interest.py
+++
@@ -1,7 +0,0 @@
-#Script that calculates the simple interest earned on an investment
-principal = 1000
-rate = 0.05
-time = 3
-interest = float(principal * rate * time)
-
-print("The simple interest is: ",interest)
