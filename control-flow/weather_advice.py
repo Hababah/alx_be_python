f6d65b8fcca39b24db99c1cc5db69245fc01edd2
Hababah <olaleyehabeebullah13@gmail.com>
2024-09-26 18:56:16 +0100

    Script that recommend weather program

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,12 @@
+# Weather Recommendation
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+if weather == 'sunny':
+    print("Wear a t-shirt and sunglasses.")
+
+elif weather == "rainy":
+    print("Dont forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf")
+else:
+    print("Sorry, I don't have recommendations for this weather ")
+# Result working perfectly
