f29ee50723ddadadd530a9dfcac871556907fd8e
Hababah <olaleyehabeebullah13@gmail.com>
2024-10-07 04:01:52 +0100

    Arithmetic Operations Script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):  # Peforming the operations
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num1 != 0:
+        if num2 == 0:
             return num1 / num2
         else:
             print("Invalid input, num1 must be greater than zero ")
