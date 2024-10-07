3635408d36ad8a5262536c5265f3ad978e171478
Hababah <olaleyehabeebullah13@gmail.com>
2024-10-07 03:05:00 +0100

    Added new directory with new file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,23 @@
+# Arithmetic_Operations.py
+
+def perform_operation(num1, num2, operation):  # Peforming the operations
+
+    match operation:
+        case "add":
+            return  num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num1 != 0:
+                return num1 / num2
+            else:
+                print("Invalid input, num1 must be greater than zero ")
+        case _:
+            print("Unrecognised input, input add/subtract/multiply/divide ")
+
+    
+
+
+
