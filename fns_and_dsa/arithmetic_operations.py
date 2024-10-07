271937e676a5ce107a842d8ca2a9ba04503dee17
Hababah <olaleyehabeebullah13@gmail.com>
2024-10-07 03:57:49 +0100

    Arithmetic Operations Script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,20 +2,19 @@
 
 def perform_operation(num1, num2, operation):  # Peforming the operations
 
-    match operation:
-        case "add":
-            return  num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num1 != 0:
-                return num1 / num2
-            else:
-                print("Invalid input, num1 must be greater than zero ")
-        case _:
-            print("Unrecognised input, input add/subtract/multiply/divide ")
+    if operation == "add":
+        return  num1 + num2
+    elif operation =="subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num1 != 0:
+            return num1 / num2
+        else:
+            print("Invalid input, num1 must be greater than zero ")
+    else:
+        print("Unrecognised input, input add/subtract/multiply/divide ")
 
     
 
